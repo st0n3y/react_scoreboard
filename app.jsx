25b527428608eb8e66cef9976c71a10d44b4a0fe
David MacKintosh <dgawmackintosh@gmail.com>
2017-02-01 11:54:59 +0000

    Mocks up UI

--- app.jsx
+++ app.jsx
@@ -1,1 +1,40 @@
-ReactDOM.render(<h1>Hello!</h1>, document.getElementById('container'));
+function Application() {
+  return (
+    <div className="scoreboard">
+      <div className="header">
+        <h1>Scoreboard</h1>
+      </div>
+    
+      <div className="players">
+        <div className="player">
+          <div className="player-name">
+            David MacKintosh
+          </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+                <div className="counter-score"> 31 </div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="players">
+        <div className="player">
+          <div className="player-name">
+            Chen Zhang
+          </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+                <div className="counter-score"> 31 </div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById('container'));
